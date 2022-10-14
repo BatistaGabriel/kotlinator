2abb0d731ba23184ad56b4aaa9cab3881405079f
Gabriel Batista <gabriel_batista@trimble.com>
2022-10-14 18:54:50 -0300

    understanding comments and incrementing ignore file

--- awesomeKotlinator/src/main/kotlin/Main.kt
+++ awesomeKotlinator/src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+/**
+ * This comment works as a documentation comment
+ * this will appear once you hover the mouse over
+ * the function name
+ */
 fun main() {
     println("Hello, World!")
+
+    // understanding how line comments works
+
+    /*
+    Understanding how multiple line
+    comments works
+     */
 }
