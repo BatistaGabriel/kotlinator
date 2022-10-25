9a060e537b6b0509a086677be4d093c59b971306
Gabriel Batista <gabriel_batista@trimble.com>
2022-10-24 21:41:15 -0300

    understanding string modifiers functions

--- awesomeKotlinator/src/main/kotlin/Main.kt
+++ awesomeKotlinator/src/main/kotlin/Main.kt
@@ -33,6 +33,8 @@ fun main() {
     helloWorldOneliner()
     println(sum(1, 2))
     println(sumOneliner(1, 2))
+
+    stringler()
 }
 
 /**
@@ -61,3 +63,28 @@ fun sum(a: Int, b: Int): Int {
 fun sumOneliner(a: Int, b: Int) = (a + b)
 
 fun helloWorldOneliner() = println("Hello World.")
+
+/**
+ * Using functions from the String class that gives you the
+ * power to modify strings as you need
+ */
+fun stringler() {
+    val myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    println("The length of the variable myString is: ${myString.length}")
+    println("The string starts with Lorem: ${myString.startsWith("Lorem")}")
+    println("The string starts with L: ${myString.startsWith('L')}")
+    println("The string ends with Lorem: ${myString.endsWith("Lorem")}")
+    println("The string ends with L: ${myString.endsWith('L')}")
+    println(
+        "The substring created starting from the [2] index position and ending on the [4] index position is: ${
+            myString.substring(
+                2,
+                4
+            )
+        }"
+    )
+    println("The string with the Lorem replaced to Truck is: ${myString.replace("Lorem", "Truck")}")
+    print("The string as lower case is: ${myString.lowercase()}")
+    print("\nThe string as upper case is: ${myString.uppercase()}")
+    print("\nThe trimmed string is: ${"                      with spaces at the start $myString".trim()}")
+}
